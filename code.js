78a6bbfbf241e155a21245aa3136b2a5ba4a9ff5
Harshita Thakur <78138193+HarshitaThakur04@users.noreply.github.com>
2021-08-10 13:37:56 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,18 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues(){
+
+    fill("white");
+    textSize(15);
+    text("R E V B A I L A",100,50);
+    fill("lightblue");
+    text("Hint: Perfoas a particular task !!",700,170);
+
+    fill("white");
+    textSize(15);
+    text("A T A D A S B A",100,310);
+    fill("lightblue");
+    text("Hint: stores all information !!",100,130);
+}
